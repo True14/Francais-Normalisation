465e69fa8d5003519fb5a94192d37f7baddd0fc1
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-06 11:30:24 -0400

    Initial travis commit

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const BearerStrategy = require('passport-http-bearer').Strategy;
 let secret = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET
-}
+};
 
-if(process.env.NODE_ENV != 'production') {
+if(process.env.NODE_ENV !== 'production') {
   secret = require('./secret');
 }
 
@@ -22,20 +22,20 @@ app.use(passport.initialize());
 
 passport.use(
     new GoogleStrategy({
-        clientID:  secret.CLIENT_ID,
-        clientSecret: secret.CLIENT_SECRET,
-        callbackURL: `/api/auth/google/callback`
+      clientID:  secret.CLIENT_ID,
+      clientSecret: secret.CLIENT_SECRET,
+      callbackURL: '/api/auth/google/callback'
     },
     (accessToken, refreshToken, profile, cb) => {
         // Job 1: Set up Mongo/Mongoose, create a User model which store the
         // google id, and the access token
         // Job 2: Update this callback to either update or create the user
         // so it contains the correct access token
-        const user = database[accessToken] = {
-            googleId: profile.id,
-            accessToken: accessToken
-        };
-        return cb(null, user);
+      const user = database[accessToken] = {
+        googleId: profile.id,
+        accessToken: accessToken
+      };
+      return cb(null, user);
     }
 ));
 
@@ -45,10 +45,10 @@ passport.use(
             // Job 3: Update this callback to try to find a user with a
             // matching access token.  If they exist, let em in, if not,
             // don't.
-            if (!(token in database)) {
-                return done(null, false);
-            }
-            return done(null, database[token]);
+          if (!(token in database)) {
+            return done(null, false);
+          }
+          return done(null, database[token]);
         }
     )
 );
@@ -58,25 +58,25 @@ app.get('/api/auth/google',
 
 app.get('/api/auth/google/callback',
     passport.authenticate('google', {
-        failureRedirect: '/',
-        session: false
+      failureRedirect: '/',
+      session: false
     }),
     (req, res) => {
-        res.cookie('accessToken', req.user.accessToken, {expires: 0});
-        res.redirect('/');
+      res.cookie('accessToken', req.user.accessToken, {expires: 0});
+      res.redirect('/');
     }
 );
 
 app.get('/api/auth/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('accessToken');
-    res.redirect('/');
+  req.logout();
+  res.clearCookie('accessToken');
+  res.redirect('/');
 });
 
 app.get('/api/me',
     passport.authenticate('bearer', {session: false}),
     (req, res) => res.json({
-        googleId: req.user.googleId
+      googleId: req.user.googleId
     })
 );
 
@@ -91,34 +91,34 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
+  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(index);
 });
 
 let server;
 function runServer(port=3001) {
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            resolve();
-        }).on('error', reject);
-    });
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      resolve();
+    }).on('error', reject);
+  });
 }
 
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        server.close(err => {
-            if (err) {
-                return reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    server.close(err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 if (require.main === module) {
-    runServer();
+  runServer();
 }
 
 module.exports = {
-    app, runServer, closeServer
+  app, runServer, closeServer
 };
