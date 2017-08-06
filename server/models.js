eeabd0097d0e9c74dc3f64c8247d7f5ab654f22b
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-06 14:48:46 -0400

    Add user model and set up config file

---
+++ server/models.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  googleId: {type: String, required: true},
+  accessToken: {type: String}
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
