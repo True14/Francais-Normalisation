11aeb1afe1383f87180c95b4f1073db14093f082
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-09 11:25:18 -0400

    Change logo name

--- client/src/components/logo.js
+++ client/src/components/logo.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import './login-page.css';
+// import './login-page.css';
 
 class Logo extends React.Component  {
     render() {
         return (
                 <div id='logo' className='logo-box'>
-                        <h1>Learningo</h1>
-                        <ul>
-                            <li>/learn-ingo/</li>
-                            <li>/learn-n-go/</li>
-                            <li>/learning-O/</li>
-                        </ul>
+                        <h1>Frenchatize me Cap{`'`}n</h1>
                 </div>
         )
     }
