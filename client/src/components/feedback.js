d7bba88e33f6fd3ff755c138705d7ac6baf68ac5
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-09 15:27:29 -0400

    Make feedback component show result

--- client/src/components/feedback.js
+++ client/src/components/feedback.js
@@ -4,22 +4,9 @@ import {connect} from 'react-redux';
 class Feedback extends React.Component {
 
   render() {
-    let result;
-    if(!this.props.currentQuestion) {
-      return <p>no questions</p>
-    }
-    if(this.props.userAnswer !== this.props.currentQuestion.answer) {
-      result = <p>Wrong</p>
-    }else {
-      result = <p>Correct!</p>
-    }
     return (
     <section className='feedback'>
-      <h2>Result</h2>
-      {result}
-      <span>Your answer: {this.props.userAnswer}</span>
-      <span>Correct answer: {this.props.currentQuestion.answer}</span>
-      <button className='submit' type='submit' onSubmit={this.onSubmit}>Next</button>
+      <h2>{this.props.currentQuestion.answer}</h2>
     </section>
    )
  }
