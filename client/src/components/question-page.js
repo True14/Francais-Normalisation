3299a72e4101f11d76a6bc855990f6eb5550039c
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-09 16:00:28 -0400

    Move answer logic to question-page

--- client/src/components/question-page.js
+++ client/src/components/question-page.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Logo from './logo';
-import {getQuestions} from '../actions';
+import {getQuestions, correct, toggleFeedback, incorrect} from '../actions';
 import Question from './question';
 import Feedback from './feedback';
 // import './question-page.css'
 
 class QuestionPage extends React.Component {
 
-    componentDidMount() {
+    componentDidMount = () => {
       this.props.dispatch(getQuestions())
+    }
+
+    _onClick = e => {
+      if (this.props.userAnswer === this.props.currentQuestion.answer) {
+        this.props.dispatch(correct())
+      }else {
+        this.props.dispatch(incorrect())
+      }
+      this.props.dispatch(toggleFeedback())
     }
 
     render() {
       let resultComponent;
-      if(!this.props.questions.length) {
+      let nextButton;
+      let result;
+      if(!this.props.currentQuestion) {
          return <p>Could not load questions</p>;
       }
 
       if(this.props.showFeedback) {
+        result = (
+          <div className='result'>
+            {this.props.result}
+          </div>
+        );
         resultComponent = <Feedback />
+        nextButton = <button className='next' type='submit' onClick={this._onClick}>Next</button>
       }
 
       return (
         <div id='question-container'>
           <Logo />
+          {result}
           <div className='question-box'>
             <Question />
           </div>
           {resultComponent}
+          {nextButton}
           <div className='logout-box'>
             <a href={'/api/auth/logout'}><button className='logout-button'>Logout</button></a>
           </div>
@@ -38,9 +57,10 @@ class QuestionPage extends React.Component {
 }
 const mapPropsToState = (state,props) => {
     return {
-        questions: state.questions,
-        score:state.score,
-        showFeedback: state.showFeedback
+        currentQuestion: state.currentQuestion,
+        userAnswer: state.userAnswer,
+        showFeedback: state.showFeedback,
+        result: state.result
     }
 }
 
