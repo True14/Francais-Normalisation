8f1ed6777385f43210f293bd90d032c19c92d660
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-10 15:12:28 -0400

    Move indes.css to css folder and import into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-// import './index.css';
+import './css/index.css';
 import {Provider} from 'react-redux';
 import store from './store';
 
