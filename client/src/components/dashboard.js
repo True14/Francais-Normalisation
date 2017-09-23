35b9ff96d6cb79b61f8f5beed1780a26c0785ba6
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-09-23 11:53:04 -0400

    Add instructions

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -9,6 +9,12 @@ function DashBoard(props) {
     <section className="dashboard-container">
       <DashHeader />
       <div className="question-container">
+        <h2>Instructions:</h2>
+        <p className="instructions">On the next page, you{`'`}ll be presented
+            with multiple examples of French vocabulary.
+            Make your best guess in the answer box.
+            Get each word correct 3 times in a row and you will complete the quiz!
+        </p>
         <p>Are you ready to begin your quiz?</p>
         <Link to="/questions">
           <button className="begin">Begin</button>
